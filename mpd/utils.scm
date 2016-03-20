8b7781853524b452549eeab1f603e97cf9776f74
Wamm K. D <jaft@brandeis.edu>
2016-03-20 00:44:03 -0500

    Replaced "first" and "second" with "list-ref"; no need to load an entire module.

--- mpd/utils.scm
+++ mpd/utils.scm
@@ -1,7 +1,6 @@
-(define-module (mpd utils)
-  :use-module (srfi srfi-1))
+(define-module (mpd utils))
 
 (define (mpd-response-list->assoc lst)
   (map (lambda (str)
          (let ([key-value (map string-trim-both (string-split str #\:))])
-           (cons (first key-value) (second key-value)))) lst))
+           (cons (list-ref key-value 1) (list-ref key-value 2)))) lst))
